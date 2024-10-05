35b05c6cbe97cfc88f39c30300f0646f104e59ff
Aniel-J Clariza <ajdcz@proton.me>
2024-10-05 14:50:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 name = input("What is your name?: ")
 print("Hello, " + name)
+
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
+sum = num1 + num2
+print("Sum: " + str(sum))
