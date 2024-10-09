be0ecf06355ab1fa769f61706f0b9ed92a2597ef
Aniel-J Clariza <ajdcz@proton.me>
2024-10-09 11:56:38 +0400

    Made Exercise1 and deleted main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name?: ")
-print("Hello, " + name)
-
-num1 = int(input("Input first number: "))
-num2 = int(input("Input second number: "))
-
-sum = num1 + num2
-print("Sum: " + str(sum))
