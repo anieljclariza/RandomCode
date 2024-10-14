366b981fcd977892859e5bd7d9fe908fc3f8940d
Aniel-J Clariza <ajdcz@proton.me>
2024-10-14 09:56:33 +0400

    Create main.py

---
+++ PrintStatementTasks/main.py
@@ -0,0 +1,65 @@
+#1. Basic String Output
+#One of the simplest ways to output or print a message to the terminal.
+print("Hello World!")
+
+#2. Simple Math Inside Print
+#I have learned that a print statement can also be used to perform arithmethic operations on integers as seen below.
+print(10 + 2)
+
+#3. I have also learned that Multiple Arguments in Print are possible by separating the arguments with a comma as seen below.
+print("Python", "is", "awesome!")
+
+
+#4. I have also learned that Print with Escape Characters can be used to output special characters like a quotation mark on the terminal as seen below.
+print("He said, \"Hi!\"")
+
+#5. I have also learned that Newline Character can be printed by using the \n to separate messages by line
+print("First line\nSecond line")
+
+#6. I have also learned that Tab Characters are made possible by using \t.
+print("Item 1\titem2\titem3")
+
+#7. I have also learned(I thought it was impossible) that Print Multiple Data Types Together is made possible by just using a comma!😮
+print("The result is", 50)
+
+#8. Print a Single Number is also possible by simply inputting the numbers and not engulfing them with quotation marks.
+print(54321)
+
+#9. Print a Decimal Number is also possible by just inputting the decimal numbers without putting quotation marks between them.
+print(3.14)
+
+#10. Print Special Characters can also be done by inputting whatever you want to output inside the quotation marks.
+print("Peso sign: ₱")
+
+#11. Using the sep Parameter can be done by using commas and whatever you want to separate the arguments with inside the double quotation marks after "sep=". This was very new to me.
+print("apple", "banana", "cherry", sep=",")
+
+#12. Using the end Parameter can be used like the one below. It just separates arguments by using end= and the thing inside that is placed after the argument before end= is placed after the argument before end=
+print("Hello,",end=" ")
+print("World!")
+
+#13. Print a Repeated String can also be done by using * and put how many times you want the message to be repeated.
+print("Python! " * 3)
+
+#14. Print a Unicode Character can be used by inputting whatever unicode character you want to output inside the double quotation marks by typing their unicode as seen below.
+print("\U0001F4BB") # Prints a computer symbol
+
+#15. Print Empty Line can be achieved by typing print() without putting anything inside those (). This can be used to provide space at your output terminal.
+print()
+
+#16. Concatenating Strings Inside is used to combine 2 or more strings by using the plus symbol.
+print("Hello " + "World!")
+
+#17. Print Boolean Values can be done by typing True or False inside the print statement.
+print(True)
+print(False)
+
+#18. Printing Exponential Notation can also be done and used to output exponents and their notations. I think this is used to add zeroes after a number.
+print(2e3)
+
+#19. Print Multiline Text Using Triple Quotes can be done by using triple quotes to make multiline statements.
+print("""This is a multiline 
+Print Statement yo.""")
+
+#20. Print Using Escaped Backslash can be done by copying the below statement.
+print("This is a backslash: \\")
