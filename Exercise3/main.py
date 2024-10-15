4975708d0b316a816a1a3551763901063f8ec6e7
Aniel-J Clariza <ajdcz@proton.me>
2024-10-15 13:12:58 +0400

    Update main.py

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,2 +1,10 @@
-name = input("What is your name?: ")
-print(name)
+a = 20
+b = 10
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Float quotient:", a / b)
+print("Integer quotient:", a // b)
+print("Remainder:", a % b)
+print("Exponent:", a ** b)
