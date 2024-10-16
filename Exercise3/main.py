189ae17f806151c4025b961d1fc59d67f0ee95a6
Aniel-J Clariza <ajdcz@proton.me>
2024-10-16 14:11:03 +0400

    Update main.py

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,10 +1,2 @@
-a = 20
-b = 10
-
-print("Sum:", a + b)
-print("Difference:", a - b)
-print("Product:", a * b)
-print("Float quotient:", a / b)
-print("Integer quotient:", a // b)
-print("Remainder:", a % b)
-print("Exponent:", a ** b)
+discount = 0.25
+print(f'{discount: .0%}')
