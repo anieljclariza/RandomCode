681d4d8cbf00369174d70d9d21dc937140995ffc
Aniel-J Clariza <ajdcz@proton.me>
2024-11-07 22:40:31 +0400

    Update StreakChecker.py

--- StreakChecker.py
+++ StreakChecker.py
@@ -1,6 +1,13 @@
 counter = int(input("What day are you on?: "))
+counter2 = int(input("What week are you on?: "))
+
 checker = (counter / 90) * 100
+checker2 = (counter2 / 13.04) * 100
+
 checker = round(checker, 2)
+checker2 = round(checker2, 2)
 
 print(str(counter) + "/90")
+print(str(counter2) + "/13")
 print("You are", str(checker) + "% done.")
+print("You are", str(checker2) + "% done.")
