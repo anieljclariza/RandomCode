1c0f0229af5ca0526f7fbb34510391ca9f019c6b
Aniel-J Clariza <ajdcz@proton.me>
2024-11-07 22:21:43 +0400

    Removed some files.

--- main.py
+++
@@ -1,6 +0,0 @@
-total_bill = float(input("Input total bill: "))
-if total_bill >= 50:
-    total_bill *= .9
-    print("Discount:", total_bill,"£")
-else:
-    print("No Discount:", total_bill,"£")
--- main4.py
+++
@@ -1,3 +0,0 @@
-nums1 = [1, 2, 3, 4, 5]
-nums1.reverse()
-print(nums1)
